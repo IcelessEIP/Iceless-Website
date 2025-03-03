5f532c35173254377e795f28ce570ce3b40d5cc0
lorenzolanglois <91885241+lorenzolanglois@users.noreply.github.com>
2025-03-03 17:38:19 +0100

    Added language support

--- js/index.js
+++ js/index.js
@@ -1,10 +1,58 @@
 "use strict"
 
+// Variables initialization
+
+const languageLinks = document.querySelectorAll("a");
+const languageNames = ["enEnglish", "frFrançais"];
+
+var lang = (window.location.href.split('/').pop() || "en");
+
+// Event listeners
+
 window.addEventListener("submit", function() {
     document.getElementById("contact-form").style.display = "none";
     document.getElementById("contact-text").style.display = "block";
     document.getElementById("detail").style.display = "none";
 });
+
+languageLinks.forEach(function(elem) {
+    elem.addEventListener("click", function (e) {
+        e.preventDefault();
+        elem.innerHTML = languageNames.find(item => item.startsWith(lang)).slice(2);
+        var oldLang = lang;
+        lang = (e.target.href.split('/').pop() || "en");
+        if (lang === "en") {
+            window.history.pushState("", "", "/");
+        } else  {
+            window.history.pushState("", "", e.target.href);
+        }
+        elem.href = oldLang === "en" ? "/" : oldLang;
+        changeLanguage(lang);
+    });
+});
+
+// Functions
+
+function changeLanguage(_lang) {
+    fetch("lang/lang-" + _lang + ".json")
+        .then(response => response.json())
+        .then(data => {
+            document.querySelectorAll('[data-translate]').forEach(el => {
+                const key = el.getAttribute('data-translate');
+                const translationText = data.text[key];
+                var isTextReplaced = false;
+
+                el.childNodes.forEach(node => {
+                  if (node.nodeType === Node.TEXT_NODE && !isTextReplaced && translationText) {
+                    node.textContent = translationText;
+                    isTextReplaced = true;
+                  }
+                });
+            });
+            document.documentElement.lang = _lang;
+            document.title = "Iceless | " + data.text["title"];
+        });
+}
 
 function main() {
     console.log("JS started");
