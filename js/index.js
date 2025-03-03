7f0971f5d2f1b11a27c1a703ae4990e12885aa43
lorenzolanglois <91885241+lorenzolanglois@users.noreply.github.com>
2025-03-03 17:16:25 +0100

    Added functional part of the email

---
+++ js/index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+window.addEventListener("submit", function() {
+    document.getElementById("contact-form").style.display = "none";
+    document.getElementById("contact-text").style.display = "block";
+    document.getElementById("detail").style.display = "none";
+});
+
+function main() {
+    console.log("JS started");
+}
+
+main();
